a019940e33212dcfe76345331fb23e1b2fb29b7a
ocostalucas <olucascosta98@gmail.com>
2020-04-07 23:11:36 -0300

    changing texts

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,14 @@ class _HomeState extends State<Home> {
 
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  String _infoText = "Informe seus dados!";
+  String _infoText = "";
 
   void _resetFields() {
     weightController.text = "";
     heightController.text = "";
 
     setState(() {
-      _infoText = "Informe seus dados!";
+      _infoText = "";
       _formKey = GlobalKey<FormState>();
     });
   }
@@ -33,18 +33,22 @@ class _HomeState extends State<Home> {
       double height = double.parse(heightController.text) / 100;
       double bmi = weight / (height * height);
 
-      if (bmi < 18.6) {
-        _infoText = "Abaixo do Peso (${bmi.toStringAsPrecision(4)})";
-      } else if (bmi >= 18.6 && bmi < 24.9) {
-        _infoText = "Peso Ideal (${bmi.toStringAsPrecision(4)})";
-      } else if (bmi >= 24.9 && bmi < 29.9) {
-        _infoText = "Levemente Acima do Peso (${bmi.toStringAsPrecision(4)})";
-      } else if (bmi >= 29.9 && bmi < 34.9) {
-        _infoText = "Obesidade Grau I (${bmi.toStringAsPrecision(4)})";
-      } else if (bmi >= 34.9 && bmi < 39.9) {
-        _infoText = "Obesidade Grau II (${bmi.toStringAsPrecision(4)})";
+      if (bmi < 16) {
+        _infoText = "Severe Thinness (${bmi.toStringAsPrecision(4)})";
+      } else if (bmi >= 16 && bmi < 17) {
+        _infoText = "Moderate Thinness (${bmi.toStringAsPrecision(4)})";
+      } else if (bmi >= 17 && bmi < 18.5) {
+        _infoText = "Mild Thinness (${bmi.toStringAsPrecision(4)})";
+      } else if (bmi >= 18.5 && bmi < 25) {
+        _infoText = "Normal (${bmi.toStringAsPrecision(4)})";
+      } else if (bmi >= 25 && bmi < 30) {
+        _infoText = "Overweight (${bmi.toStringAsPrecision(4)})";
+      } else if (bmi >= 30 && bmi < 35) {
+        _infoText = "Obese Class I (${bmi.toStringAsPrecision(4)})";
+      } else if (bmi >= 35 && bmi < 40) {
+        _infoText = "Obese Class II (${bmi.toStringAsPrecision(4)})";
       } else if (bmi >= 40) {
-        _infoText = "Obesidade Grau III (${bmi.toStringAsPrecision(4)})";
+        _infoText = "Obese Class III (${bmi.toStringAsPrecision(4)})";
       }
     });
   }
@@ -53,7 +57,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Calculadora de IMC"),
+          title: Text("BMI Calculator"),
           centerTitle: true,
           backgroundColor: Colors.green,
           actions: <Widget>[
@@ -72,28 +76,28 @@ class _HomeState extends State<Home> {
                   TextFormField(
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(
-                        labelText: "Peso (Kg)",
+                        labelText: "Weight (kg)",
                         labelStyle: TextStyle(color: Colors.green)),
                     textAlign: TextAlign.center,
                     style: TextStyle(color: Colors.green, fontSize: 25.0),
                     controller: weightController,
                     validator: (value) {
                       if (value.isEmpty) {
-                        return "Insira seu peso!";
+                        return "Enter your weight!";
                       }
                     },
                   ),
                   TextFormField(
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(
-                        labelText: "Altura (cm)",
+                        labelText: "Height (cm)",
                         labelStyle: TextStyle(color: Colors.green)),
                     textAlign: TextAlign.center,
                     style: TextStyle(color: Colors.green, fontSize: 25.0),
                     controller: heightController,
                     validator: (value) {
                       if (value.isEmpty) {
-                        return "Insira sua altura!";
+                        return "Enter your height!";
                       }
                     },
                   ),
@@ -103,7 +107,7 @@ class _HomeState extends State<Home> {
                           height: 50.0,
                           child: RaisedButton(
                             onPressed: () {
-                              if(_formKey.currentState.validate()){
+                              if (_formKey.currentState.validate()) {
                                 _calculate();
                               }
                             },
